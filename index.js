0e5ae96bc5425bb2a33dca3287d3e34c7bc6869c
jessonfoo <jessonfoo@gmail.com>
2017-11-06 22:50:25 -0800

    update index.js => adjust for testing constraints

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 /* eslint-disable brace-style */
 const sqlite = require('sqlite'),
-      Sequelize = require('sequelize'),
-      request = require('then-request'),
-      Promise = require('bluebird'),
-      express = require('express'),
-      app = express();
+Sequelize = require('sequelize'),
+request = require('then-request'),
+Promise = require('bluebird'),
+express = require('express'),
+app = express();
 
-const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
+const {PORT = 3000, NODE_ENV = 'development', DB_PATH = './db/database.db'} = process.env;
 
 // START SERVER
 Promise.resolve()
-  .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
-  .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
+.then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
+.catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
 // define sequelize
-const sequelize = new Sequelize('main','username','password', {
+const sequelize = new Sequelize('main', 'username', 'password', {
   dialect: 'sqlite',
   storage: DB_PATH,
   port   : 3306
@@ -51,71 +51,107 @@ function getAverageReviewRatingByFilmId(filmId) {
 }
 
 function getRatings(filmsArray) {
-  return Promise.each(filmsArray,function(film) {
-    return getAverageReviewRatingByFilmId(parseInt(film.id)).then( rating => {
+  return Promise.each(filmsArray, function (film) {
+    return getAverageReviewRatingByFilmId(parseInt(film.id)).then(rating => {
       film.average_rating = rating;
       return film;
     });
   });
 }
+
 function getRelatedFilms(filmId) {
   return Promise.resolve(
-    Film.sync()
-    .then(function() {
-      return Film.find({where: {id: filmId}});
-    })
-    .then(filmQueryResponse => {
-      // parse film query
-      let film = filmQueryResponse.get({simple: true});
-      console.log(film);
-      // define the genre id && parse int
-      let genreId = parseInt(film.genreId);
-      // use the film release date to create a new Date that we can use to query with
-      let d = new Date(film.release_date);
-      let day = d.getDay();
-      let month = d.getMonth();
-      let year = d.getFullYear();
-      let releaseMin = new Date(year - 15, month, day);
-      let releaseMax = new Date(year + 15, month, day);
-      // do a new query for all films of same genre id
-      return Film.findAll({
-        where: {
-          release_date:{
-            $lt: releaseMax,
-            $gt: releaseMin
-          },
-          genre_id: genreId
-        }
-      }).then(films => {
-        let filmsArray = films.map(f => f.toJSON());
-        return filmsArray;
-      });
-    })
+  Film.sync()
+  .then(function () {
+    return Film.find({where: {id: filmId}});
+  })
+  .then(filmQueryResponse => {
+    // parse film query
+    let film = filmQueryResponse.get({simple: true});
+    // define the genre id && parse int
+    let genreId = parseInt(film.genreId);
+    // use the film release date to create a new Date that we can use to query with
+    let d = new Date(film.release_date);
+    let day = d.getDay();
+    let month = d.getMonth();
+    let year = d.getFullYear();
+    let releaseMin = new Date(year - 15, month, day);
+    let releaseMax = new Date(year + 15, month, day);
+    // do a new query for all films of same genre id
+    return Film.findAll({
+      where: {
+        release_date: {
+          $lt: releaseMax,
+          $gt: releaseMin
+        },
+        genre_id    : genreId
+      }
+    }).then(films => {
+      let filmsArray = films.map(f => f.toJSON());
+      return filmsArray;
+    });
+  })
   );
 }
+
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
   try {
-   let params = req.params;
-   let filmId = parseInt(params.id);
-    sequelize.sync().then(function() {
+    let params = req.params;
+    let paramKeys = Object.keys(params);
+    let limit = 10;
+    let offset = 0;
+    let filmId = false;
+    if (paramKeys.indexOf('limit') !== -1) {
+      if (params.limit && parseInt(params.limit) > 0) {
+        limit = params.limit;
+      } else {
+        throw Error("error:invalid limit");
+      }
+    }
+    if (paramKeys.indexOf('offset') !== -1) {
+      if (parseInt(params.offset) > 0 && params.offset) {
+        offset = params.offset;
+      } else {
+        throw Error("error:invalid offset");
+      }
+    }
+    if (paramKeys.indexOf('id') !== -1) {
+      if (parseInt(params.id) > 0 && params.id) {
+        filmId = params.id;
+      } else {
+        throw Error("error:invalid id");
+      }
+    }
+    sequelize.sync().then(function () {
       getRelatedFilms(filmId).then(films =>
-        getRatings(films))
-      .then( recommendations => {
-        let rFilms = recommendations.filter(function(film){
+      getRatings(films))
+      .then(recommendations => {
+      });
+    }).done(recommendations=>{
+      let rFilms = [];
+      if(Array.isArray(recommendations)){
+         rFilms = recommendations.filter(function (film) {
           return (parseFloat(film.average_rating) > 4);
+        });
+      }
+      return res.status(200).json({
+        recommendations: rFilms,
+        meta           : {
+          limit : limit,
+          offset: offset
+        }
       });
-        return res.status(200).json({recommendations:rFilms});
-      });
+
     });
 
   }
-  catch(e) {
-    if(e && e.message) {
-      res.status(400).send('Error: ' + e.message);
+  catch (e) {
+    if (e && e.message) {
+      res.status(400).json({error: e.message});
     } else {
       console.log(e);
-      res.status(500).send('Internal Server Error: Please Check Logs');
+      res.status(500).json({error: 'Internal Server Error: Please Check Logs'});
     }
 
   }
