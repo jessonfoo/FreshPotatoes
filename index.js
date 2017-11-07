09676aabd3509b25899f3032386ae5def288e861
jessonfoo <jessonfoo@gmail.com>
2017-11-06 17:04:30 -0800

    initialize repo

---
+++ index.js
@@ -0,0 +1,22 @@
+const sqlite = require('sqlite'),
+      Sequelize = require('sequelize'),
+      request = require('request'),
+      express = require('express'),
+      app = express();
+
+const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
+
+// START SERVER
+Promise.resolve()
+  .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
+  .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
+
+// ROUTES
+app.get('/films/:id/recommendations', getFilmRecommendations);
+
+// ROUTE HANDLER
+function getFilmRecommendations(req, res) {
+  res.status(500).send('Not Implemented');
+}
+
+module.exports = app;
